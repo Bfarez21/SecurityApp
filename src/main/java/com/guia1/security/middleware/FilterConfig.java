5193b698e1aae8ce0abad44bb8a219e36210d816
Bryan <bryanfareznieves@gmail.com>
2025-05-08 11:34:08 -0500

    Create FilterConfig
    Registramos el filtro

--- src/main/java/com/guia1/security/middleware/FilterConfig.java
+++ src/main/java/com/guia1/security/middleware/FilterConfig.java
@@ -1,4 +1,18 @@
 package com.guia1.security.middleware;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class FilterConfig {
+
+    @Bean
+    public FilterRegistrationBean<UrlFilter> urlFilterRegistration(){
+        FilterRegistrationBean<UrlFilter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(new UrlFilter());
+        registrationBean.addUrlPatterns("/api/crypto/*");
+        registrationBean.setOrder(1); // prioridad en la cadena de filtros
+        return registrationBean;
+    }
 }
